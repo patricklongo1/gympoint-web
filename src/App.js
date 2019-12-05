282b23e936813094d11c8104fdafe846d6f4887b
Patrick Longo <ck.longo@bol.com.br>
2019-12-05 14:36:06 -0300

    routes configured

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import GlobalStyle from './styles/global';
+
+import Routes from './routes';
+import history from './services/history';
 
 function App() {
   return (
-    <h1>GymPoint</h1>
+    <Router history={history}>
+      <GlobalStyle />
+      <ToastContainer autoClose={3000} />
+      <Routes />
+    </Router>
   );
 }
 export default App;
---
+++ src/routes/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Switch } from 'react-router-dom';
+import Route from './Route';
+
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SingUp';
+
+import StudentList from '../pages/StudentList';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={SignIn} />
+      <Route path="/register" component={SignUp} />
+
+      <Route path="/student" component={StudentList} isPrivate />
+      <Route path="/" component={() => <h1>Error 404 - Not Found</h1>} />
+    </Switch>
+  );
+}
