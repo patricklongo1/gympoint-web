4a5b0ce06dafa63ce6034a3d68c28f614d52bffd
Patrick Longo <ck.longo@bol.com.br>
2019-12-05 19:16:31 -0300

    header done

--- src/pages/StudentList/index.js
+++ src/pages/StudentList/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import Header from '../../components/Header';
 
 export default function StudentList() {
   return (
     <>
-      <Header />
       <h1>Lista de alunos</h1>
     </>
   );
